23cea1444a2044bc1454da34696b4c8cc9be879c
flizzyflo <vip3r1@gmx.de>
2023-11-12 14:17:05 +0100

    introduced new function

--- main.py
+++ main.py
@@ -23,8 +23,38 @@ def get_relevant_html() -> tuple[ResultSet]:
     return selection_divs, correct_answer_divs
 
 
+def extract_questions_from_attempt(attempt: int) -> None:
+    """
+    _summary_
+
+    Args:
+        attempt (int): the attemptnumber to extract the questions from
+    """    
+    PARAMS['attempt'] = attempt
+    response = requests.get(URL, 
+                            cookies=COOKIES, 
+                            headers=HEADERS, 
+                            params=PARAMS)
+    
+    if not response.status_code == 200:
+        print(f"Could not connecto to {URL} for attempt {attempt} - request is aborted. HTTP Answer-Statuscode: {response.status_code}")
+        return
+    
+    question_divs, correct_answer_divs = get_relevant_html()
+    extractor = QuestionAnswerImageExtractor(question_divs=question_divs,
+                                                correct_answers_divs=correct_answer_divs,
+                                                image_storage_path=IMAGE_FILE_PATH)
+
+    extractor.generate_results()  
+    raw_result = extractor.get_results()
+
+    FileHandler.store_results(results=raw_result, 
+                                questions_csv_file_path=CSV_FILE_PATH)
+
+
 if __name__ == "__main__":
 
+    # variable declaration
     attempts: list[str]
     correct_answer_divs: ResultSet
     question_divs: ResultSet
@@ -33,28 +63,35 @@ if __name__ == "__main__":
     raw_result: dict[str, str]
     clean_results: dict[str, str]
     
+    # selected course attempt
     attempts = COURSES[COURSE_NUMBER]
+
+    # loop through all atempts in a single course unit
     for attempt in attempts:
-        PARAMS['attempt'] = attempt
-        response = requests.get(URL, 
-                                cookies=COOKIES, 
-                                headers=HEADERS, 
-                                params=PARAMS)
-        
-        if not response.status_code == 200:
-            print(f"Could not connecto to {URL} for attempt {attempt} - request is aborted. HTTP Answer-Statuscode: {response.status_code}")
-            continue
-        
-        question_divs, correct_answer_divs = get_relevant_html()
-        extractor = QuestionAnswerImageExtractor(question_divs=question_divs,
-                                                 correct_answers_divs=correct_answer_divs,
-                                                 image_storage_path=IMAGE_FILE_PATH)
+        extract_questions_from_attempt(attempt=attempt)
 
-        extractor.generate_results()  
-        raw_result = extractor.get_results()
 
-        FileHandler.store_results(results=raw_result, 
-                                  questions_csv_file_path=CSV_FILE_PATH)
+    
+        # PARAMS['attempt'] = attempt
+        # response = requests.get(URL, 
+        #                         cookies=COOKIES, 
+        #                         headers=HEADERS, 
+        #                         params=PARAMS)
+        
+        # if not response.status_code == 200:
+        #     print(f"Could not connecto to {URL} for attempt {attempt} - request is aborted. HTTP Answer-Statuscode: {response.status_code}")
+        #     continue
+        
+        # question_divs, correct_answer_divs = get_relevant_html()
+        # extractor = QuestionAnswerImageExtractor(question_divs=question_divs,
+        #                                          correct_answers_divs=correct_answer_divs,
+        #                                          image_storage_path=IMAGE_FILE_PATH)
+
+        # extractor.generate_results()  
+        # raw_result = extractor.get_results()
+
+        # FileHandler.store_results(results=raw_result, 
+        #                           questions_csv_file_path=CSV_FILE_PATH)
 
         #clean_results = filter_duplicates_from_scraped_results(results=raw_result, 
         #                                                       questions_csv_file_path=CSV_FILE_PATH)

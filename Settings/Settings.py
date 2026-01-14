e2fbac95080bdf81639591c52fcf4a7ad6cb530d
flizzyflo <vip3r1@gmx.de>
2026-01-14 22:48:17 +0100

    added spaces

--- Settings/Settings.py
+++ Settings/Settings.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 COURSES: Dict[int, List[int]] = {187091: [1088549, 1088798, 1088802, 1088804, 1088809, 1088816, 1088822, 1088825, 1088830, 1088838, 1088842, 1088845, 1088850, 1088855, 1088859, 1088541],
                                  187106: [1088861, 1088864, 1088867, 1088871, 1088877, 1088880, 1088887, 1088889, 1088893, 1088898, 1088902, 1088912, 1088919, 1088923, 1088927, 1088934]}
+
 COURSE_NAMES: Dict[int, str] = {187091: "Bayes-Klassifikation",
                                 187106: "Entscheidungswälder"}
+
 URL: str = "https://moodle.fernuni-hagen.de/mod/quiz/review.php"
 
 COURSE_NUMBER: str = 187091

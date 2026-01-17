50211643711839c79cd0b73fd12f7e67723a6beb
flizzyflo <vip3r1@gmx.de>
2026-01-17 11:44:07 +0100

    added courses

--- Settings/Settings.py
+++ Settings/Settings.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 from pathlib import Path
+import os
 
 # cmid: List[attempts] -> beides aus URL entnommen
 COURSES: Dict[int, List[int]] = {187091: [1088549, 1088798, 1088802, 1088804, 1088809, 1088816, 1088822, 1088825, 1088830, 1088838, 1088842, 1088845, 1088850, 1088855, 1088859, 1088541],
@@ -8,7 +9,9 @@ COURSES: Dict[int, List[int]] = {187091: [1088549, 1088798, 1088802, 1088804, 10
                                  187133: [1093807, 1093811, 1093818, 1093826, 1093862, 1093871, 1093884, 1093891, 1093898, 1093901, 1093906, 1093908],
                                  187144: [1093913, 1093916, 1093919, 1093923, 1093926, 1093928, 1093932, 1093934, 1093936, 1093940, 1093945, 1093949],
                                  187150: [1093957, 1094013, 1094017, 1094019, 1094022, 1094026, 1094028, 1094032, 1094034, 1094036, 1094040, 1094042, 1094043],
-                                 187165: [1094045, 1094055, 1094058, 1094064, 1094071, 1094075, 1094080, 1094086, 1094091, 1094094, 1094097, 1094100]}
+                                 187165: [1094045, 1094055, 1094058, 1094064, 1094071, 1094075, 1094080, 1094086, 1094091, 1094094, 1094097, 1094100],
+                                 187177: [1094196, 1096706, 1096716, 1096718, 1096731, 1096748, 1096761, 1096767, 1096773, 1096778, 1096784, 1096787, 1096795, 1096804, 1096811],
+                                 187187: [1096838, ]}
 
 COURSE_NAMES: Dict[int, str] = {187091: "Bayes-Klassifikation",
                                 187106: "Entscheidungswälder",
@@ -16,14 +19,16 @@ COURSE_NAMES: Dict[int, str] = {187091: "Bayes-Klassifikation",
                                 187133: "Hierarchisches_Clustering",
                                 187144: "Assoziationsregeln",
                                 187150: "Anomalieerkennung",
-                                187165: "Hauptkomponentenanalyse"}
+                                187165: "Hauptkomponentenanalyse",
+                                187177: "Markov Entscheidungsprozesse",
+                                187187: "Passives Reinforcement Learning"}
 
 URL: str = "https://moodle.fernuni-hagen.de/mod/quiz/review.php"
 
-COURSE_NUMBER: int = 187165
+COURSE_NUMBER: int = 187177
 
 # aus cookies im browser
-COOKIES: Dict[str, str] = {"MoodleSession": "kginonfu6vsm0333jsf7pvls6g",
+COOKIES: Dict[str, str] = {"MoodleSession": "ljshps588r0hi6kba2v85jlv6c",
                            "MDL_SSP_SessID": "c7fd86140d34082734cc193fdbbdf0dd"}
 
 HEADERS: Dict[str, str] = {"User-Agent": "Mozilla/5.0",
@@ -32,5 +37,6 @@ HEADERS: Dict[str, str] = {"User-Agent": "Mozilla/5.0",
 PARAMS: Dict[str, int] = {"attempt": 0}
 CSV_FILE_PATH: Path = Path(f"questions.csv")
 IMAGE_FILE_PATH: Path = Path("/images")
+FILEPATH_FOR_CSV=Path(f"results{os.sep}{COURSE_NAMES[COURSE_NUMBER]}_{CSV_FILE_PATH}")
 DELIMITER: str = ";"
 QUESTION_ID: int = 1

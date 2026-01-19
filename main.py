db35e4c1999ca64a5ffeaddd8a54827d56dc8de1
flizzyflo <vip3r1@gmx.de>
2026-01-19 19:59:50 +0100

    refactored variable names

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 
 
 from Workers.FileWriting import FileHandler
-from Settings.Settings import  COURSE_NUMBER, FILEPATH_FOR_CSV,COURSE_NAMES
+from Settings.Settings import  CURRENT_COURSE_NUMBER, FILEPATH_FOR_CSV,COURSE_NAMES
 from utils.AttemptExtractor import get_questions_for
 
 
 
 if __name__ == "__main__":
 
-    get_questions_for(course_number=COURSE_NUMBER)
+    get_questions_for(course_number=CURRENT_COURSE_NUMBER)
 
     print("-" * 20)
     FileHandler.filter_duplicates_from_stored_file(FILEPATH_FOR_CSV)
     print("-" * 20)
-    print(f"Extractions for course '{COURSE_NUMBER}: {COURSE_NAMES[COURSE_NUMBER]}' are done.")
+    print(f"Extractions for course '{CURRENT_COURSE_NUMBER}: {COURSE_NAMES[CURRENT_COURSE_NUMBER]}' are done.")
